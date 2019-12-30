2adb4ba299eb33c0762e7c977d006323ef74664c
Samlet Wu <xiaofei.wu@gmail.com>
2019-12-30 09:52:11 +0800

    add streamlit: parsers

--- sl_parsers.py
+++ sl_parsers.py
@@ -79,7 +79,7 @@ def get_subj_domain(sent):
     return rs
 
 
-class value(object):
+class OperValue(object):
 
     def __init__(self, vtype=None, left=None, op=None, right=None, alias=None):
         self.alias = alias
@@ -89,14 +89,14 @@ class value(object):
         self._right = right
 
     def __lt__(self, other):
-        return value(self._type, self._left, '$lt', other, self.alias)
+        return OperValue(self._type, self._left, '$lt', other, self.alias)
 
     # //	__floordiv__(self, other)
     def __floordiv__(self, other):
-        return value(self._type, self, '$sub', other, self.alias)
+        return OperValue(self._type, self, '$sub', other, self.alias)
 
     def __rshift__(self, other):
-        return value(self._type, self, '$for', other, self.alias)
+        return OperValue(self._type, self, '$for', other, self.alias)
 
     #     def __getattr__(self, name):
     #         if self._type:
@@ -107,21 +107,82 @@ class value(object):
     #         else:
     #             return value(self.alias, name)
     def __call__(self, *args):
-        return value(self._type, self._left, op='predicate', right=args, alias=self.alias)
+        return OperValue(self._type, self._left, op='predicate', right=args, alias=self.alias)
 
 
-class closure(object):
+class Closure(object):
     def __getattr__(self, name):
-        return value(name)
+        return OperValue(name)
 
+def desc_expr(el, domains, lang):
+    from sagas.nlu.inspector_wordnet import predicate
+
+    if el._type.startswith('__'):
+        comp='__'
+        sub = el._type
+        oper=el._type[2:]
+    elif '_' in el._type:
+        parts=el._type.split('_')
+        comp=f"{parts[0]}'s {parts[1]}"
+        sub=parts[0]
+        oper=parts[1]
+    else:
+        comp=f"{el._type}'s lemma"
+        sub=el._type
+        oper='equals'
+    val=', '.join(el._right)
+    cond=eval_path(domains, sub)
+    eq=lambda vals: any([v in vals for v in cond.split('/')])
+    oper_mappings={'equals': eq,
+                   'text': eq,
+                   'cat': lambda vals: any([predicate(e, cond, lang, '*') for e in vals])
+                   }
+    if oper in oper_mappings:
+        result=oper_mappings[oper](el._right)
+        msg=''
+    else:
+        result=False
+        msg=f'invalid operator {oper}'
+    st.markdown(f"- `{comp}` {el._op} `{val}`, *{cond}* -> {result} {msg}")
+
+    return result, msg
+
+def eval_path(domains, path):
+    from jsonpath_ng import jsonpath, parse
+
+    if path.startswith('__'):
+        return domains['lemma']+'/'+domains['text']
+    else:
+        prefix='$.'
+        suffix='.text,lemma'
+        parts=path.split('/')
+        parts_str='.'.join([f"{t}[*]" for t in parts])
+        expr= f"{prefix}{parts_str}{suffix}"
+        parser=parse(expr)
+        word = '/'.join([match.value for match in parser.find(domains)])
+        return word
+
+def predicate(domains, val, lang):
+    results=[]
+    st.markdown(f"*{val._left._type}* {val._left._op} {val._left._right}")
+    results.append(desc_expr(val._left, domains, lang))
+
+    # desc_expr(val._left)
+    expr=val._right
+    if isinstance(expr, list):
+        for el in expr:
+            results.append(desc_expr(el, domains, lang))
+    else:
+        results.append(desc_expr(expr, domains, lang))
+    return results
 
 def sure(doc, val):
     st.write('-' * 10)
     st.write(val._left._type, val._op, val._right._type)
     st.write(val._left._op, val._left._right, val._right._op, val._right._right)
 
-ud = closure()
-def testing(doc):
+ud = Closure()
+def testing(doc, lang):
     # m = value('$m')
     # s = value('$s')
     # aux = value('$aux')
@@ -132,6 +193,9 @@ def testing(doc):
     # sure(aux('is') // nsubj('animal'))
     for el in doc:
         st.markdown(f"`{el['lemma']}` >> *{el['dc']['lemma']}*")
+        r1=predicate(el, ud.__text('will') >> [ud.nsubj('what'), ud.dc_cat('weather')], lang)
+        # r2=predicate(el, ud.__cat('be') >> [ud.nsubj('what'), ud.dc_cat('animal/object')], lang)
+        st.write([r[0] for r in r1], all([r[0] for r in r1]))
 
     # sure(doc, ud.aux('is') // ud.nsubj('animal'))
     sure(doc, ud.aux('is') >> ud.nsubj('animal'))
@@ -147,7 +211,7 @@ def parse_aux():
     doc_jsonify, resp = parse_sents(data)
 
     domains = get_aux_domain(doc_jsonify)
-    testing(domains)
+    testing(domains, 'en')
 
     # show analyse graph
     gv = display_doc_deps(doc_jsonify, resp)
@@ -161,10 +225,13 @@ def parse_subj():
     st.write(sents)
     data = {'lang': lang, "sents": sents, 'engine': cf.engine(lang)}
     doc_jsonify, resp = parse_sents(data)
+
+    domains = get_subj_domain(doc_jsonify)
+    testing(domains, 'ru')
+
     gv=display_doc_deps(doc_jsonify, resp)
     st.graphviz_chart(gv)
 
-    domains = get_subj_domain(doc_jsonify)
     st.write(domains)
 
 def main():
