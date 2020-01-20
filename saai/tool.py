86a97821b2f4be4e8aecbf7a0072e64a1860ae85
Samlet Wu <xiaofei.wu@gmail.com>
2020-01-20 14:37:27 +0800

    add notebooks: ner, parser

--- saai/tool.py
+++ saai/tool.py
@@ -12,17 +12,17 @@ def ner_jieba_vis(text):
     spans = [(w['start'], w['end'], w['entity']) for w in ner_jieba(text)]
     show_box_markup(text, spans, palette=palette(PER=BLUE, ORG=RED, LOC=GREEN))
 
-def rasa_nlu_parse(sents) -> Dict[Text, Any]:
-    response = requests.post(f'http://localhost:5005/model/parse', json={'text': sents})
+def rasa_nlu_parse(sents:Text, host_and_port:Text='http://localhost:5005') -> Dict[Text, Any]:
+    response = requests.post(f'{host_and_port}/model/parse', json={'text': sents})
     if response.status_code!=200:
         logger.warning(f'status code is {response.status_code}, fail to parse {sents}')
         return {}
     return response.json()
 
-def rasa_nlu_vis(sents):
+def rasa_nlu_vis(sents, host_and_port:Text='http://localhost:5005'):
     from ipymarkup import show_box_markup
 
-    result = rasa_nlu_parse(sents)
+    result = rasa_nlu_parse(sents, host_and_port=host_and_port)
     if len(result)>0:
         ents = result['entities']
         spans = [(w['start'], w['end'], w['entity']) for w in ents]
