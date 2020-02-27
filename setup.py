b64ff8d876a861d3e6029c14ef367592ff39d6e6
Samlet Wu <xiaofei.wu@gmail.com>
2020-02-27 17:40:11 +0800

    fix scripts

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ with open("README.md", "r") as fh:
 
 # What packages are required for this module to be executed?
 REQUIRED = [
+    "colorlog",
     "jieba~=0.40",
     "simplejson",
     "paddlepaddle-tiny~=1.6.1",
